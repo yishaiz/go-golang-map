dfe2599f08e0cac2f0eb29b8d53eee35e1b60a1a
yishai <yishai5555@gmail.com>
2020-11-08 13:53:24 +0200

    02-delete"

--- main.go
+++ main.go
@@ -9,5 +9,24 @@ func main() {
 		"blue":  "0000ff",
 	}
 
+	colors["white"] = "#ffffff"
+
+	// colors2 := make(map[string]string)
+
 	fmt.Println(colors)
+	delete(colors, "white")
+	fmt.Println(colors)
+
+	colors2 := map[int]string{
+		11: "#ff0000",
+		12: "40ff05",
+		13: "0000ff",
+	}
+
+	colors2[10] = "#f0f0f0"
+
+	fmt.Println(colors2)
+	delete(colors2, 10)
+	fmt.Println(colors2)
+
 }
