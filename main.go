207b05dfd3263a8e3d9d2ef89feb750fa65a5f4d
yishai <yishai5555@gmail.com>
2020-11-08 14:01:47 +0200

    05-iterate-over-map

--- main.go
+++ main.go
@@ -6,27 +6,40 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "40ff05",
-		"blue":  "0000ff",
+		"white": "ffffff",
 	}
 
-	colors["white"] = "#ffffff"
-
-	// colors2 := make(map[string]string)
-
-	fmt.Println(colors)
-	delete(colors, "white")
-	fmt.Println(colors)
-
-	colors2 := map[int]string{
-		11: "#ff0000",
-		12: "40ff05",
-		13: "0000ff",
-	}
-
-	colors2[10] = "#f0f0f0"
-
-	fmt.Println(colors2)
-	delete(colors2, 10)
-	fmt.Println(colors2)
-
+	printMap(colors)
 }
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, " is ", hex)
+	}
+}
+
+// colors := map[string]string{
+// 	"red":   "#ff0000",
+// 	"green": "40ff05",
+// 	"blue":  "0000ff",
+// }
+
+// colors["white"] = "#ffffff"
+
+// // colors2 := make(map[string]string)
+
+// fmt.Println(colors)
+// delete(colors, "white")
+// fmt.Println(colors)
+
+// colors2 := map[int]string{
+// 	11: "#ff0000",
+// 	12: "40ff05",
+// 	13: "0000ff",
+// }
+
+// colors2[10] = "#f0f0f0"
+
+// fmt.Println(colors2)
+// delete(colors2, 10)
+// fmt.Println(colors2)
